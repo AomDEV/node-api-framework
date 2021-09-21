65011c907bb6e713c48f2bc13527f99dd002cde4
Siriwat Janke <siriwat576@gmail.com>
2021-09-22 01:21:42 +0700

    add jwt middleware

--- src/api/v1/example/route.ts
+++ src/api/v1/example/route.ts
@@ -1,9 +1,11 @@
 import {makeHandler} from "@/core/express/router";
 import express from "express"
+import {RequiredToken} from "@/framework/entity/auth"
 import getData from "@/api/v1/example/category/get-data";
 import getAll from "./category/get-all";
 const router = express.Router();
 
+//router.use(RequiredToken());
 router.get('/something', makeHandler(getAll));
 router.get('/something/:id', makeHandler(getData));
 
