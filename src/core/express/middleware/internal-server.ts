bff0548993bf0e3b64de26c5eb77ee488221880e
Siriwat Janke <siriwat576@gmail.com>
2021-09-22 01:25:54 +0700

    rename middleware file import

--- src/core/express/middleware/internal-server.ts
+++ src/core/express/middleware/internal-server.ts
@@ -1,4 +1,4 @@
-import middleware from "@/core/express/middleware"
+import middleware from "@/core/express/custom-middleware"
 import internalServer from "@/core/express/error/internal-server";
 export default middleware(function(request: Express.Request, setStatus: Function) {
     setStatus(500);
