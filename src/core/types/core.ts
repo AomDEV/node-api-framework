66e6acb6d0e3c69bcd04ad53b2eab6efbb9ffb01
Siriwat Janke <siriwat576@gmail.com>
2021-09-22 04:42:39 +0700

    add new redis type configuration

--- src/core/types/core.ts
+++ src/core/types/core.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 type ExpressHandler = (request: express.Request, response: express.Response) => Promise<void>;
 type ExpressMiddleware = (request: express.Request, response: express.Response, next: express.NextFunction) => void;
+type RedisOptions = {ttl?:number, name?:string};
+type MakeHandlerOptions = {useCache?:boolean,redis?:RedisOptions};
+type RedisCacheResponse = {data:any, code:number};
 
-export {ExpressHandler, ExpressMiddleware};
+export {ExpressHandler, ExpressMiddleware, MakeHandlerOptions, RedisCacheResponse};
