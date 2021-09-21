3f980d0bbe758e5e40d6bb14b1d2395dc0230e59
Siriwat Janke <siriwat576@gmail.com>
2021-09-22 01:26:24 +0700

    rename custom middleware file import

--- src/core/express/middleware/not-found.ts
+++ src/core/express/middleware/not-found.ts
@@ -1,4 +1,4 @@
-import middleware from "@/core/express/middleware"
+import middleware from "@/core/express/custom-middleware"
 import notFound from "@/core/express/error/not-found";
 export default middleware(function(request: Express.Request, setStatus: Function) {
     setStatus(404);
