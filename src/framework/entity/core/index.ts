4cb0d14000f0adb4eff06625c79ef2864847efb9
Siriwat Janke <siriwat576@gmail.com>
2021-09-22 17:37:53 +0700

    add console-debug export from index.ts

--- src/framework/entity/core/index.ts
+++ src/framework/entity/core/index.ts
@@ -2,4 +2,6 @@ import IsOverridedStatus from "./is-overrided-status";
 import ExceptionBuilder from "./exception-builder";
 import MiddlewareExceptionBuilder from "./middleware-exception";
 import DebugError from "./debug-error";
-export {IsOverridedStatus, ExceptionBuilder, MiddlewareExceptionBuilder, DebugError};
+import cd from "./console-debug";
+
+export {IsOverridedStatus, ExceptionBuilder, MiddlewareExceptionBuilder, DebugError, cd};
