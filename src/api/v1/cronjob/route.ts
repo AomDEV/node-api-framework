5f6cf3a02dec9100f1276e656dc5bb6305cfda9b
Siriwat Janke <siriwat576@gmail.com>
2021-09-22 12:16:46 +0700

    create cronjob ready folder

---
+++ src/api/v1/cronjob/route.ts
@@ -0,0 +1,13 @@
+import {makeHandler} from "@/core/express/router";
+import express from "express"
+import getAll from "@/api/v1/example/category/get-all";
+import getData from "@/api/v1/example/category/get-data";
+const router = express.Router();
+
+/** Cronjob Routes **/
+
+
+
+/********************/
+
+export default router;
