923f671e846d48f7cc8969d766b29da3aa6a1b23
Siriwat Janke <siriwat576@gmail.com>
2021-09-22 17:47:21 +0700

    edit test cases

--- src/__tests__/example.spec.ts
+++ src/__tests__/example.spec.ts
@@ -2,14 +2,14 @@ import app from "../app";
 import request from 'supertest';
 
 describe("An example api endpoint", () => {
-    it('Get an example api endpoint', (done: any) => {
+    it('Test something all', (done: any) => {
         request(app.unitTest())
             .get('/api/v1/example/something')
             .set('Accept', 'application/json')
             .expect({status:true,data:{callback:7.000000000000001}})
             .expect(200, done)
     });
-    it('Get an example api endpoint', (done: any) => {
+    it('Test something ID #1', (done: any) => {
         request(app.unitTest())
             .get('/api/v1/example/something/1')
             .set('Accept', 'application/json')
