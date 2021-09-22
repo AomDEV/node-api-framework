4702473ff1a6a3616663f0e737b94546c3a93ebb
Siriwat Janke <siriwat576@gmail.com>
2021-09-22 17:35:04 +0700

    remove unused code

--- src/api/v1/example/category/get-all.ts
+++ src/api/v1/example/category/get-all.ts
@@ -3,7 +3,6 @@ import {example} from '@/framework/entity/example'
 import { useCache } from '@/framework/entity/redis'
 
 export default async function (request: express.Request){
-    let x = async (a:string)=>"test " + a;
     let callback = await useCache(example().CalculateTaxFee, 100, 7);
     return {
         callback: callback,
