ca89a3e7ad62370ba1553a28f2c64d82d8bd76c6
Siriwat Janke <siriwat576@gmail.com>
2021-09-24 20:58:17 +0700

    database pagination

--- src/core/database/base-database.ts
+++ src/core/database/base-database.ts
@@ -1,5 +1,6 @@
-import { PrismaClient } from ".prisma/client";
 import prisma from "@/core/database"; 
+import CustomError from "@/core/exception";
+import { PrismaPaginationHeader } from "@/core/types";
 
 class BaseDatabase {
     protected prisma = prisma;
@@ -7,11 +8,40 @@ class BaseDatabase {
     public table(): any{
         return null;
     }
-
-    public findAll(){
-        return this.table().findMany();
+    public findAll(Options?: any){
+        return this.table().findMany(Options);
     }
-    public findBy(Query: any, Options: any = null){
+    public async findAllPagination(CurrentPage: number, Limit: number = 10, Options?: any){
+        const page = CurrentPage || 1;
+        const limit = Limit || 10;
+        const startIndex = (page - 1) * limit;
+        const totalCount = await this.table().count();
+        const totalPage = Math.ceil(totalCount / limit);
+        const currentPage = page || 0;
+        const header: PrismaPaginationHeader = {totalCount:0, totalPage:0, currentPage:0, limit:0};
+        let result: any[] = [];
+        try{
+            header.totalCount = totalCount;
+            header.totalPage = totalPage;
+            header.currentPage = currentPage;
+            header.limit = limit;
+
+            if(page <= 0 || page > totalPage) {
+                throw new CustomError("Resource not found");
+            } else{
+                result = await this.table().findMany({
+                    take: limit,
+                    skip: startIndex,
+                    ...Options
+                });
+            }
+        } catch (err) {
+            return {result, header};
+        } finally{
+            return {result, header};
+        }
+    }
+    public findMany(Query: any, Options: any = null){
         return this.table().findMany({
             where: Query,
             ...Options
@@ -70,7 +100,7 @@ class BaseDatabase {
         });
     }
     public truncate(){
-        this.table().deleteMany({});
+        this.table().deleteMany();
     }
 }
 export default BaseDatabase;
